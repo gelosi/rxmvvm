3d358e3ae28435890525fb8ffc0e175387e848ec
Oleg Shanyuk <oleg.shanyuk@gmail.com>
2019-01-21 00:41:02 +0100

    extend restaurant coordinate api for easy use on pair with CLLocationCoordinate2D

--- rxmvvm/Restaurant.swift
+++ rxmvvm/Restaurant.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Oleg Shanyuk. All rights reserved.
 //
 
+import MapKit
 import UIKit
 
 class Restaurant: Codable {
@@ -13,10 +14,19 @@ class Restaurant: Codable {
     class Coordinate: Codable {
         let latitude: Double
         let longitude: Double
+
+        lazy var coordinate2d = {
+            CLLocationCoordinate2D(latitude: self.latitude, longitude: self.longitude)
+        }
         
         init(lat: Double, lon: Double) {
             self.latitude = lat
             self.longitude = lon
+        }
+
+        init(coordinate2d: CLLocationCoordinate2D) {
+            self.latitude = coordinate2d.latitude
+            self.longitude = coordinate2d.longitude
         }
     }
     
